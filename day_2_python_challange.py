e44792dfb832063227282cf306153640675cf680
Wawrzyniak <ewawrzyniak@cps.k12.il.us>
2024-09-16 15:09:13 -0500

    grammer + names

--- day_2_python_challange.py
+++ day_2_python_challange.py
@@ -5,8 +5,7 @@ income= input("How much did you make today?")
 
 comision = (int(income)*13/100)
 
-print ("Your name is " + name + " and you have collected " + str(income) + " which means you have made " + str(comision) + " in comissions.")
+print ("Your name is " + name + " and you have collected $" + str(income) + " which means you have made $" + str(comision) + " in comissions.")
 
 # Yao
 # Emily 
-
