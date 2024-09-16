505f809e498177d5a5065331cb2b39d15f08de88
Wawrzyniak <ewawrzyniak@cps.k12.il.us>
2024-09-16 14:55:24 -0500

    first commit

---
+++ day_2_python_challange.py
@@ -0,0 +1,11 @@
+name= input("What is your name?")
+print("Your name is:"+ name)
+#input for total sales 
+income= input("How much did you make today?")
+
+comision = (int(income)*13/100)
+
+print ("Your name is " + name + " and you have collected " + str(income) + " which means you have made " + str(comision) + " in comissions.")
+
+
+
