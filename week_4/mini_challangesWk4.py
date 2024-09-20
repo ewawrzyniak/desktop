64be5da3475ccf07824c5a1aa552da51dec9e9d2
Wawrzyniak <ewawrzyniak@cps.k12.il.us>
2024-09-20 15:02:50 -0500

    string, join, seperate

---
+++ week_4/mini_challangesWk4.py
@@ -0,0 +1,45 @@
+#Emily 
+
+# String Methods Practice #1
+#slides 12 -16
+# Print the following text in uppercase, using the specific string method:
+# "Especially in electronic communications, writing in all caps is equivalent to yelling."
+# sentence = "Especially in electronic communications, writing in all caps is equivalent to yelling."
+# print (sentence.upper()) #prints the sentece in uppercase
+# #lowercase method in python
+# sentence2= "ESPN IS THE BEST SPORTS NETEWORK"
+# print (sentence2.lower()) #writes the sentence in all lowercase 
+# print (sentence.find("communications")) #prints the index of the worrd communications
+# print (sentence[26:39]. upper()) #prints the word communication in uppercase
+# print (sentence.replace("communications", "COMMUNICATIONS")) #makes the word communications all 
+
+# String Methods Practice #2
+# Join the following list into a string, separating each item with a space. Use the appropriate list/string method, and display the result.
+# word_list = ["Simple","is","better","than","complex."]
+
+
+
+# String Methods Practice #3
+# Replace in the following sentence:
+# "If the implementation is hard to explain, it might be a bad idea."
+# the following pairs of words:
+# "hard" --> "easy"
+# "bad" --> "good"
+# and display the sentence with both words modified.
+
+#################################string properties################################
+
+# String Properties Practice #1
+# Concatenate the text "Repetition" 15 times and display the result on the screen.
+# Luckily, you know that strings are multipliable and you can do this activity in a simple and elegant way.
+
+# String Properties Practice #2
+# Check if the word "beach" is not found in the following haiku. You should print the boolean.
+# "Whitecaps on the bay:
+# A broken signboard banging
+# In the April wind."
+# — Richard Wright, collected in Haiku: This Other World, 1998
+
+
+# String Properties Practice #3
+# Check the Python Documentation to find the description of the len() function. Then, display on the screen the length (in number of characters) of the word electroencephalographist.
